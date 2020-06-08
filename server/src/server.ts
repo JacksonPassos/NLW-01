4a228c074a127ce5f93cddaa76ee287bc38f550e
JacksonPassos <jacksonguitar33@gmail.com>
2020-06-07 21:13:38 -0300

    criado banco de dados e migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (req, res)=> {
-  console.log("listagem de usuários")
-  return res.json([
-    'Diego',
-    'Cleiton',
-    'Robson'
-  ])
-})
+app.use(express.json())
+app.use(routes)
+
 
 app.listen('3333')
